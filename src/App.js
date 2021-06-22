396371fedb7bbf313f3716be20f24d33499cf00b
Dayo Awobeku <dawobeku@gmail.com>
2021-06-22 21:37:38 +0100

    Routing next

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import "./App.css";
-import Navbar from "./Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HeroSection from "./Hero Section/HeroSection";
-import SecondSection from "./Second Section/SecondSection";
-import ThirdSection from "./Third Section/ThirdSection";
-import FourthSection from "./Fourth Section/Fourth-section";
-import FourthSectionButton from "./Fourth Section/FourthSectionButton";
-import Footer from "./Footer/Footer";
+import Homepage from "./Homepage";
+import LogIn from "./LogIn";
+import GetStarted from "./GetStarted";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        <HeroSection />
-        <SecondSection />
-        <ThirdSection />
-        <FourthSection />
-        <FourthSectionButton />
-        <Footer />
-
         <Switch>
-          <Route path="/" exact></Route>
+          <Route path="/" exact>
+            <Homepage />
+          </Route>
+          <Route path="/log-in" exact>
+            <LogIn />
+          </Route>
+          <Route path="/get-started" exact>
+            <GetStarted />
+          </Route>
         </Switch>
       </Router>
     </>
---
+++ src/GetStarted.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+
+function GetStarted() {
+  return (
+    <div>
+      <Navbar />
+      <h2>SignUp screen</h2>
+      <h2>SignUp screen</h2>
+      <h2>SignUp screen</h2>
+      <h2>SignUp screen</h2>
+      <h2>SignUp screen</h2>
+      <h2>SignUp screen</h2>
+    </div>
+  );
+}
+
+export default GetStarted;
