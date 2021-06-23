d40f8db2a2b3fad6c2c6ed205e024b46fed8bbe1
Dayo Awobeku <dawobeku@gmail.com>
2021-06-23 22:53:21 +0100

    Form authentication

---
+++ src/Form Authentication/useForm.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useForm = (validate) => {
+  const [values, setValues] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors(validate(values));
+  };
+
+  return { handleChange, values, handleSubmit, errors };
+};
+
+export default useForm;
--- src/Get Started/GetStarted.js
+++ src/Get Started/GetStarted.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./GetStarted.css";
+import useForm from "../Form Authentication/useForm";
+import validate from "../Form Authentication/validateInfo";
 
 function GetStarted() {
+  const { handleChange, values, handleSubmit, errors } = useForm(validate);
+
   return (
     <div className="overall-container-get-started">
-      <img
-        className="get-started-logo"
-        src="https://raw.githubusercontent.com/awobekuD/PTJ-90-Project/6e1cf155e609208bf310e0bcacffa3a8641d2ae5/src/images/Get%20Started%20Logo.svg"
-        alt="escrow-logo"
-      />
+      <Link to="/PTJ-90-Project">
+        <img
+          className="get-started-logo"
+          src="https://raw.githubusercontent.com/awobekuD/PTJ-90-Project/6e1cf155e609208bf310e0bcacffa3a8641d2ae5/src/images/Get%20Started%20Logo.svg"
+          alt="escrow-logo"
+        />
+      </Link>
+
+      {/* for local 3000 */}
+      {/* <Link to="/">
+        <img
+          className="get-started-logo"
+          src="https://raw.githubusercontent.com/awobekuD/PTJ-90-Project/6e1cf155e609208bf310e0bcacffa3a8641d2ae5/src/images/Get%20Started%20Logo.svg"
+          alt="escrow-logo"
+        />
+      </Link> */}
 
       <div className="get-started-container">
         <h2>Create your account</h2>
@@ -18,42 +33,69 @@ function GetStarted() {
           <h4 className="business">Business</h4>
           <h4>Customer</h4>
         </div>
-        <form action="POST" className="get-started-form">
-          <label htmlFor="fname">Full Name</label>
-          <input type="text" id="fname" name="fname" />
+        <form
+          action="POST"
+          onSubmit={handleSubmit}
+          className="get-started-form"
+        >
+          <label htmlFor="fullname">Full Name</label>
+          <input
+            type="text"
+            id="fullname"
+            name="fullname"
+            value={values.fullname}
+            onChange={handleChange}
+          />
+          {errors.fullname && <p>{errors.fullname}</p>}
 
           <label htmlFor="email" className="email">
             Email
           </label>
-          <input type="text" id="email" name="email" />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
 
           <label htmlFor="password" className="password">
             Password
           </label>
-          <input type="password" id="password" name="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+
           <img
             src="https://raw.githubusercontent.com/awobekuD/PTJ-90-Project/75852d3203c16ecb8bad52c52946233f6c766cd7/src/images/Hide%20password%20icon.svg"
             alt="show/hide password"
             className="show-password"
           />
 
-          <p className="user-agreement">
+          <h5 className="user-agreement">
             By continuing, you agree to our{" "}
             <Link to="/user-agreement" className="user-agreement-link">
               <span className="text-blue">User Agreement</span> and{" "}
               <span className="text-blue">Privacy Policy</span>{" "}
             </Link>
-          </p>
-        </form>
-        <div className="actions">
-          <Link to="/get-started" className="get-started-button">
-            Create my account
-          </Link>
+          </h5>
 
-          <Link to="/log-in" className="log-in">
-            Log In
-          </Link>
-        </div>
+          <div className="actions">
+            <button type="submit" className="get-started-button">
+              Create my account
+            </button>
+
+            <Link to="/log-in" className="log-in">
+              Log In
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
