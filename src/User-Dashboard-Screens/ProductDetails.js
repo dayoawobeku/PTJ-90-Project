789f9328f5fc2e5bf1d30e379830c732719c77be
Shiguruji <80973804+Shiguruji@users.noreply.github.com>
2021-07-10 20:32:12 +0100

    added responsiveness for product details page

--- src/User-Dashboard-Screens/ProductDetails.js
+++ src/User-Dashboard-Screens/ProductDetails.js
@@ -17,16 +17,16 @@ const ProductDetails = () => {
             <div className="price-and-qty">
               <div className="price product-details-labels">
                 <label htmlFor="price">Price</label>
-                <input type="number" min="1000" step="100" />
+                <input type="number" className="price-input" min="1000" step="100" />
               </div>
 
               <div className="qty product-details-labels">
                 <label htmlFor="quantity">Quantity</label>
-                <input type="number" name="quantity" min="1" max="10" />
+                <input type="number" className="quantity-input" name="quantity" min="1" max="10" />
               </div>
             </div>
 
-            <div className="action">
+            <div className="product-action">
               <Link to="/payment" className="next-btn">
                 Next
               </Link>
